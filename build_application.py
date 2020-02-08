3e6e488aef225948bde78ed6a4bab3c71fa43f01
Pedro Lemos <lemozxpedro@gmail.com>
2020-02-08 18:49:52 -0300

    Updated build script to change the version

--- build_application.py
+++ build_application.py
@@ -1,23 +1,24 @@
 import os
 from cx_Freeze import setup, Executable
 
-target = Executable (
+target = Executable(
     script = "Magnet Link Catcher.py",
     base = "Win32GUI",
     icon = "icon.ico"
-    )
+)
 
-setup (
+setup(
     name = "Magnet Link Catcher",
-    version = "1.0",
+    version = "1.2",
     description = "Get magnet links from internet without any effort!",
     author = "Pedro Lemos",
     options = {
-                "build_exe": {
-                        "packages": ["os", "tkinter", "PySimpleGUI", "pyperclip", "requests", "urllib.parse", "bs4"],
-                        "include_files": ["icon.ico", "icon.png"]
-                    }
-            },
+        "build_exe": {
+            "packages": ["os", "tkinter", "PySimpleGUI", "pyperclip", "requests", "urllib.parse", "bs4", "json"],
+            "include_files": ["icon.ico", "icon.png"],
+            'include_msvcr': True
+        }
+    },
     
     executables = [target]
-    )
+)
