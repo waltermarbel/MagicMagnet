db0ca6cf2d228cd8668af307334a9ae7ae323aaa
Pedro Lemos <lemozxpedro@gmail.com>
2021-07-31 12:18:08 -0300

    project: wiped everything

--- lib/app/core/error/failures.dart
+++
@@ -1,5 +0,0 @@
-import 'package:magic_magnet_engine/magic_magnet_engine.dart';
-
-class UnsupportedPlatformFailure extends Failure {
-  UnsupportedPlatformFailure() : super('This platform is not supported yet');
-}
